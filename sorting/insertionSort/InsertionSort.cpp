e62c6dcd103bc36baf8a7d2dc9ccd6dc477bf836
Alex Ray <alexray@rgnt1-22-136-dhcp.int.colorado.edu>
2017-01-20 12:56:39 -0700

    Added comments

--- sorting/insertionSort/InsertionSort.cpp
+++ sorting/insertionSort/InsertionSort.cpp
@@ -6,6 +6,14 @@ using namespace std;
 /*
 Insertion sort takes a reference to an input array of integers
 Sorts array using insertion sort algorithm
+
+Basic insertion sort algorithm:
+Start with an array, containing a sorted subarray and an unsorted subarray
+    Initial sorted subarray consists of arr[0] (the first element)
+Each iteration, move one element from the unsorted subarray to the sorted
+    Take element from unsorted and place in correct position in sorted
+
+The algorithm is finished when there are no more unsorted elements
 */
 void InsertionSort::insertionSort(int arr[], int size) {
     // if size is 0 or 1, the array is already sorted
@@ -17,7 +25,7 @@ void InsertionSort::insertionSort(int arr[], int size) {
         // Iterate through all unsorted elements
         for (int i = 1; i < size; i++) {
             int key = arr[i]; // Next element to place in sorted array
-            int j = i - 1;
+            int j = i - 1; // j is index of the end of the sorted subarray
             
             // Iterate backwards through sorted subarray until key is in correct location
             while (j >= 0 && arr[j] > key) {
