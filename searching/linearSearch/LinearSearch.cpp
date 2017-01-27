3fb2612aff459d767c773b6737faeee9822a860f
Alex Ray <alexray@rgnt1-23-2-dhcp.int.colorado.edu>
2017-01-27 13:08:39 -0700

    Added linear search description

--- searching/linearSearch/LinearSearch.cpp
+++ searching/linearSearch/LinearSearch.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+/*
+Linear search takes an array of ints, size of array, and target
+Linear search runs in O(n) time
+    Worst case, the algorithm searches through n elements
+
+Basic linear search algorithm:
+Ensure array is not empty
+Sequentially search through every element in array until target is found or the entire array has been searched
+Returns the index of target, or -1 if target is not in array
+*/
 int LinearSearch::linearSearch(int arr[], int size, int target) {
     // If array is empty return -1
     if (size == 0) {
