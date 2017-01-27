9b8b12e2535dfafba2e736b312178f8edd5a854d
Alex Ray <alexray@rgnt1-23-2-dhcp.int.colorado.edu>
2017-01-27 13:15:49 -0700

    Added examples for insertion sort

--- sorting/insertionSort/main.cpp
+++ sorting/insertionSort/main.cpp
@@ -15,7 +15,8 @@ void print(int arr[], int size) {
 
 int main() {
     InsertionSort *sort = new InsertionSort();
-
+    
+    // Example 1
     int arr[] = {3, 5, 1, 2, 6, 5, 6};
     int size = sizeof(arr)/sizeof(arr[0]); // Size of array in bytes/# bytes     per element
     cout << "Unsorted array: " << endl;
@@ -25,4 +26,26 @@ int main() {
 
     cout << "Sorted array: " << endl;
     print(arr, size);
+    cout << endl;
+    // Example 2
+    int arr2[] = {1,2,3,4,5,4,3,2,12,3};
+    size = sizeof(arr2)/sizeof(arr2[0]); // Size of array in bytes/# bytes     per element
+    cout << "Unsorted array: " << endl;
+    print(arr2, size);
+
+    sort->insertionSort(arr2, size);
+
+    cout << "Sorted array: " << endl;
+    print(arr2, size);
+    cout << endl;
+    // Example 3
+    int arr3[] = {1,1,1,2,3,4,5,10};
+    size = sizeof(arr3)/sizeof(arr3[0]); // Size of array in bytes/# bytes     per element
+    cout << "Unsorted array: " << endl;
+    print(arr3, size);
+
+    sort->insertionSort(arr3, size);
+
+    cout << "Sorted array: " << endl;
+    print(arr3, size);
 }
